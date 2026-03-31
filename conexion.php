4422708a98740ccdedc727705c378daf5e9af4a2
Camila <lchavesac.01@gmail.com>
2026-03-31 17:35:30 -0500

    Base en la nube conectada con el codigo

--- conexion.php
+++ conexion.php
@@ -1,12 +1,26 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$password = "";
-$baseDatos = "lectoescriturabd";
+$host = "mysql-duolinguistico-dev-lcnc28032510.mysql.database.azure.com";
+$usuario = "admin_data";
+$password = "Database123";
+$database = "lectoescritura_test";
 
-$conexion = new mysqli($servidor, $usuario, $password, $baseDatos);
+$conexion = mysqli_init();
 
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
+mysqli_ssl_set($conexion, NULL, NULL, NULL, NULL, NULL);
+
+mysqli_real_connect(
+    $conexion,
+    $host,
+    $usuario,
+    $password,
+    $database,
+    3306,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
+
+// Verificar conexión
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
 }
 ?>
